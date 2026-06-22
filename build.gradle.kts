5200556d352795d28673a30d63fc908248bcfa28
Jon Brighton <brightonsbox@hotmail.com>
2026-06-22 15:50:12 +0100

    Upgrading hmpps-gradle-spring-boot to 10.5.1 (#82)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.1"
   kotlin("jvm") version "2.4.0"
   kotlin("plugin.spring") version "2.4.0"
   jacoco
@@ -21,7 +21,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-jackson")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("io.sentry:sentry-spring-boot-4:8.43.2")
+  implementation("io.sentry:sentry-spring-boot-4:8.44.1")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
   constraints {
     implementation("org.webjars:swagger-ui:5.32.2")
