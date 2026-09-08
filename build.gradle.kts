2e27c418a48d9a2d6e0cc932827c4b0e3dcc3969
Igor Ushkarev <ushkarev@gmail.com>
2026-09-08 19:38:53 +0100

    Use the default swagger-ui version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("io.sentry:sentry-spring-boot-4:8.54.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
-  constraints {
-    implementation("org.webjars:swagger-ui:5.32.11")
-  }
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:3.0.1")
   testImplementation("org.springframework.boot:spring-boot-starter-jackson-test")
